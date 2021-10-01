a875b3d37b6d22d27a8f4f079ace7659733388c2
Francisco Rutayebesibwa <fmruta98@gmail.com>
2021-10-01 16:15:41 +0200

    feat: Initial Layout

---
+++ src/components/Student.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+
+<div class="student">
+<slot name="name"></slot>
+<slot name="description"></slot>
+</div>
+
+<style>
+    .student{
+        box-shadow: 10px 5px 5px black;
+        margin: 30px;
+}
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+let name = 'world';
+import Student from '../components/Student.svelte';
+const jobs = ["job1", "job2", "job3"];
+</script>
+
+<nav>
+Our logo
+<div class="links">
+<a href="#">Home</a>
+<a href="#">Jobs</a>
+<a href="#">Students</a>
+<a href = "#">Login</a>
+</div>
+
+</nav>
+
+{#each jobs as job}
+<Student >
+    <div slot = "name">
+    Home
+    </div>
+    <div slot = "description">
+    Cape Town
+    </div>
+</Student>
+{/each}
+
+<style>
+nav {
+display: flex;
+justify-content: space-between;
+padding: 10px 30px;
+background: #fafafa;
+}
+a {
+margin: 0 20px;
+padding: 7px;
+text-decoration: none;
+}
+a:hover {
+background: #d0d4d9;
+}
+</style>
